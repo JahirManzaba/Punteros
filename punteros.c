16bf9362afca3d3bce65553c57fd840ac7e44bcb
Jahir Manzaba 3 E <jahir.manzaba@udla.edu.ec>
2026-05-05 11:43:43 -0500

    Programa básico de punteros en C

---
+++ punteros.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {    
+    int num = 15, a;
+    int x;       
+    x = &num;       
+    a = *x;        
+    printf("%p %d",x, a);
+char *ptr = 'a', c;
+char *ptr;
+ptr = &letra;
+c=*ptr;
+printf("%p %c",ptr, c);
+    return 0;
+}
